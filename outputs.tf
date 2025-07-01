c3a70a1ef8e05b899310a8093f520cd666e2fe84
vivek muthuvelu <vivek.muthuvelu@gmail.com>
2025-07-01 08:25:08 -0500

    Switch to using tfe_outputs-2

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,11 @@
 # OUTPUT
 ##################################################################################
 
-output "vpc_id" {
+output "tfe_vpcid" {
   value       = module.main.vpc_id
   description = "VPC iD"
 
 }
-
 output "public_subnets" {
   value       = module.main.public_subnets
   description = "list of public subnets"
